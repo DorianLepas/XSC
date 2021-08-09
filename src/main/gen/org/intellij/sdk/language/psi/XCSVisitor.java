75df0d2a4131355abc519f2b264f821774521481
DL792526 <dorian.lepas@gmail.com>
2021-08-09 12:07:50 +0200

    Gen class

---
+++ src/main/gen/org/intellij/sdk/language/psi/XCSVisitor.java
@@ -0,0 +1,170 @@
+// This is a generated file. Not intended for manual editing.
+package org.intellij.sdk.language.psi;
+
+import org.jetbrains.annotations.*;
+import com.intellij.psi.PsiElementVisitor;
+import com.intellij.psi.PsiElement;
+
+public class XCSVisitor extends PsiElementVisitor {
+
+  public void visitAsciiValues4(@NotNull XCSAsciiValues4 o) {
+    visitPsiElement(o);
+  }
+
+  public void visitAsciiValues5(@NotNull XCSAsciiValues5 o) {
+    visitPsiElement(o);
+  }
+
+  public void visitCeCore(@NotNull XCSCeCore o) {
+    visitPsiElement(o);
+  }
+
+  public void visitCeList(@NotNull XCSCeList o) {
+    visitPsiElement(o);
+  }
+
+  public void visitCeProperty(@NotNull XCSCeProperty o) {
+    visitPsiElement(o);
+  }
+
+  public void visitCeProperty_(@NotNull XCSCeProperty_ o) {
+    visitPsiElement(o);
+  }
+
+  public void visitDvCore(@NotNull XCSDvCore o) {
+    visitPsiElement(o);
+  }
+
+  public void visitDvList(@NotNull XCSDvList o) {
+    visitPsiElement(o);
+  }
+
+  public void visitDvProperty(@NotNull XCSDvProperty o) {
+    visitPsiElement(o);
+  }
+
+  public void visitDvProperty_(@NotNull XCSDvProperty_ o) {
+    visitPsiElement(o);
+  }
+
+  public void visitEcCore(@NotNull XCSEcCore o) {
+    visitPsiElement(o);
+  }
+
+  public void visitEcList(@NotNull XCSEcList o) {
+    visitPsiElement(o);
+  }
+
+  public void visitEcProperty(@NotNull XCSEcProperty o) {
+    visitPsiElement(o);
+  }
+
+  public void visitEcProperty_(@NotNull XCSEcProperty_ o) {
+    visitPsiElement(o);
+  }
+
+  public void visitEventsCore(@NotNull XCSEventsCore o) {
+    visitPsiElement(o);
+  }
+
+  public void visitFunctionCore(@NotNull XCSFunctionCore o) {
+    visitPsiElement(o);
+  }
+
+  public void visitProperty(@NotNull XCSProperty o) {
+    visitPsiElement(o);
+  }
+
+  public void visitProperty_(@NotNull XCSProperty_ o) {
+    visitPsiElement(o);
+  }
+
+  public void visitScenariosCore(@NotNull XCSScenariosCore o) {
+    visitPsiElement(o);
+  }
+
+  public void visitSitCore(@NotNull XCSSitCore o) {
+    visitPsiElement(o);
+  }
+
+  public void visitSitList(@NotNull XCSSitList o) {
+    visitPsiElement(o);
+  }
+
+  public void visitSvCore(@NotNull XCSSvCore o) {
+    visitPsiElement(o);
+  }
+
+  public void visitSvList(@NotNull XCSSvList o) {
+    visitPsiElement(o);
+  }
+
+  public void visitSvProperty(@NotNull XCSSvProperty o) {
+    visitPsiElement(o);
+  }
+
+  public void visitSvProperty_(@NotNull XCSSvProperty_ o) {
+    visitPsiElement(o);
+  }
+
+  public void visitVssCore(@NotNull XCSVssCore o) {
+    visitPsiElement(o);
+  }
+
+  public void visitVssList(@NotNull XCSVssList o) {
+    visitPsiElement(o);
+  }
+
+  public void visitVssProperty(@NotNull XCSVssProperty o) {
+    visitPsiElement(o);
+  }
+
+  public void visitVssProperty_(@NotNull XCSVssProperty_ o) {
+    visitPsiElement(o);
+  }
+
+  public void visitCollectionEventSection(@NotNull XCSCollectionEventSection o) {
+    visitPsiElement(o);
+  }
+
+  public void visitDataVariableSection(@NotNull XCSDataVariableSection o) {
+    visitPsiElement(o);
+  }
+
+  public void visitEquipmentConstantSection(@NotNull XCSEquipmentConstantSection o) {
+    visitPsiElement(o);
+  }
+
+  public void visitEventsSection(@NotNull XCSEventsSection o) {
+    visitPsiElement(o);
+  }
+
+  public void visitFunctions(@NotNull XCSFunctions o) {
+    visitPsiElement(o);
+  }
+
+  public void visitScenariosSection(@NotNull XCSScenariosSection o) {
+    visitPsiElement(o);
+  }
+
+  public void visitSecsItemTypeSection(@NotNull XCSSecsItemTypeSection o) {
+    visitPsiElement(o);
+  }
+
+  public void visitStandartSections(@NotNull XCSStandartSections o) {
+    visitPsiElement(o);
+  }
+
+  public void visitStatusVariableSection(@NotNull XCSStatusVariableSection o) {
+    visitPsiElement(o);
+  }
+
+  public void visitVfeiSecsSeqSection(@NotNull XCSVfeiSecsSeqSection o) {
+    visitPsiElement(o);
+  }
+
+  public void visitPsiElement(@NotNull PsiElement o) {
+    visitElement(o);
+  }
+
+}
---
+++ src/main/gen/org/intellij/sdk/language/psi/impl/XCSEventsCoreImpl.java
@@ -0,0 +1,42 @@
+// This is a generated file. Not intended for manual editing.
+package org.intellij.sdk.language.psi.impl;
+
+import java.util.List;
+import org.jetbrains.annotations.*;
+import com.intellij.lang.ASTNode;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiElementVisitor;
+import com.intellij.psi.util.PsiTreeUtil;
+import static org.intellij.sdk.language.psi.XCSTypes.*;
+import com.intellij.extapi.psi.ASTWrapperPsiElement;
+import org.intellij.sdk.language.psi.*;
+
+public class XCSEventsCoreImpl extends ASTWrapperPsiElement implements XCSEventsCore {
+
+  public XCSEventsCoreImpl(@NotNull ASTNode node) {
+    super(node);
+  }
+
+  public void accept(@NotNull XCSVisitor visitor) {
+    visitor.visitEventsCore(this);
+  }
+
+  @Override
+  public void accept(@NotNull PsiElementVisitor visitor) {
+    if (visitor instanceof XCSVisitor) accept((XCSVisitor)visitor);
+    else super.accept(visitor);
+  }
+
+  @Override
+  @Nullable
+  public XCSAsciiValues4 getAsciiValues4() {
+    return findChildByClass(XCSAsciiValues4.class);
+  }
+
+  @Override
+  @NotNull
+  public List<XCSEventsCore> getEventsCoreList() {
+    return PsiTreeUtil.getChildrenOfTypeAsList(this, XCSEventsCore.class);
+  }
+
+}
