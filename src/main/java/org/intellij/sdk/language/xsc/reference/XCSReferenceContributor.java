b7f438477af9508d5b3b6b7b305102b1ce0f1661
m2gi-lepasd <dorian.lepas@etu.univ-grenoble-alpes.fr>
2021-10-13 11:25:02 +0200

    Save

--- src/main/java/org/intellij/sdk/language/xsc/reference/XCSReferenceContributor.java
+++ src/main/java/org/intellij/sdk/language/xsc/reference/XCSReferenceContributor.java
@@ -4,6 +4,7 @@ import com.intellij.openapi.util.TextRange;
 import com.intellij.patterns.PlatformPatterns;
 import com.intellij.psi.*;
 import com.intellij.util.ProcessingContext;
+import org.intellij.sdk.language.xsc.psi.XCSProperty_;
 import org.jetbrains.annotations.NotNull;
 
 public class XCSReferenceContributor extends PsiReferenceContributor {
@@ -21,11 +22,33 @@ public class XCSReferenceContributor extends PsiReferenceContributor {
                         if ((value != null && value.startsWith("xsc" + ":"))) {
                             TextRange property = new TextRange("xsc".length() + ":".length() + 1,
                                     value.length() + 1);
+                            //System.out.println(" value = " + value + "\n property" + property);
                             return new PsiReference[]{new XCSReference(element, property)};
                         }
                         return PsiReference.EMPTY_ARRAY;
                     }
                 });
     }
-
+    /*
+    @Override
+    public void registerReferenceProviders(@NotNull PsiReferenceRegistrar registrar) {
+        System.out.println("JE PASSE 1");
+        registrar.registerReferenceProvider(PlatformPatterns.psiElement(XCSProperty_.class),
+                new PsiReferenceProvider() {
+                    @Override
+                    public PsiReference @NotNull [] getReferencesByElement(@NotNull PsiElement element,
+                                                                           @NotNull ProcessingContext context) {
+                        System.out.println("JE PASSE 2");
+                        XCSProperty_ PropName = (XCSProperty_) element;
+                        String value = PropName.getValue();
+                        if ((value != null)) {
+                            TextRange property = new TextRange(0,value.length());
+                            System.out.println(" value = " + value + "\n property" + property);
+                            return new PsiReference[]{new XCSReference(element, property)};
+                        }
+                        return PsiReference.EMPTY_ARRAY;
+                    }
+                });
+    }
+    */
 }
