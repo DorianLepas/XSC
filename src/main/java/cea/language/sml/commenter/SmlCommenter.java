b1765585a6dc67e177452d44bda7500cbe86f725
DL792526 <dorian.lepas@gmail.com>
2021-10-28 13:56:07 +0200

    Added Sml commenter

---
+++ src/main/java/cea/language/sml/commenter/SmlCommenter.java
@@ -0,0 +1,38 @@
+package cea.language.sml.commenter;
+
+import com.intellij.lang.Commenter;
+import org.jetbrains.annotations.Nullable;
+
+public class SmlCommenter implements Commenter {
+
+    @Nullable
+    @Override
+    public String getLineCommentPrefix() {
+        return "//";
+    }
+
+    @Nullable
+    @Override
+    public String getBlockCommentPrefix() {
+        return "/*";
+    }
+
+    @Nullable
+    @Override
+    public String getBlockCommentSuffix() {
+        return "*/";
+    }
+
+    @Nullable
+    @Override
+    public String getCommentedBlockCommentPrefix() {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public String getCommentedBlockCommentSuffix() {
+        return null;
+    }
+
+}
