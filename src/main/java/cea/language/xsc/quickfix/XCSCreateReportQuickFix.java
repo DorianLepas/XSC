640d8e8a5753854d70b806a3b5bbb8001c98792d
DL792526 <dorian.lepas@gmail.com>
2021-11-26 16:56:25 +0100

    Started report quick fix

---
+++ src/main/java/cea/language/xsc/quickfix/XCSCreateReportQuickFix.java
@@ -0,0 +1,93 @@
+package cea.language.xsc.quickfix;
+
+import cea.language.xsc.filetype.XCSFileType;
+import cea.language.xsc.psi.*;
+import com.intellij.codeInsight.intention.impl.BaseIntentionAction;
+import com.intellij.lang.ASTNode;
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.command.WriteCommandAction;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.fileEditor.FileEditorManager;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.pom.Navigatable;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.PsiManager;
+import com.intellij.psi.search.FileTypeIndex;
+import com.intellij.psi.search.GlobalSearchScope;
+import com.intellij.psi.tree.TokenSet;
+import com.intellij.util.IncorrectOperationException;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Collection;
+import java.util.Objects;
+
+public class XCSCreateReportQuickFix extends BaseIntentionAction {
+
+    private final String key;
+    private final PsiElement element;
+
+    public XCSCreateReportQuickFix(String key, PsiElement element) {
+        this.key = key;
+        this.element = element;
+    }
+
+    @NotNull
+    @Override
+    public String getText() {
+        return "Create report '" + key + "'";
+    }
+
+    @NotNull
+    @Override
+    public String getFamilyName() {
+        return "Create report";
+    }
+
+    @Override
+    public boolean isAvailable(@NotNull Project project, Editor editor, PsiFile file) {
+        return file instanceof XCSFile;
+    }
+
+    @Override
+    public void invoke(@NotNull final Project project, final Editor editor, PsiFile file) throws
+            IncorrectOperationException {
+        ApplicationManager.getApplication().invokeLater(() -> {
+            XCSFile xcsFile = (XCSFile) file;
+            if (xcsFile != null) {
+                // Create Report in the file
+                createProperty(project, xcsFile.getVirtualFile());
+
+            }
+        });
+    }
+
+    private void createProperty(final Project project, final VirtualFile file) {
+        WriteCommandAction.writeCommandAction(project).run(() -> {
+            ASTNode lastChildNode = null;
+            // Create a report
+            XCSFile xcsFile = (XCSFile) PsiManager.getInstance(project).findFile(file);
+            ASTNode @NotNull [] children = Objects.requireNonNull(xcsFile).getNode().getChildren(TokenSet.ANY);
+            for (ASTNode child : children) {
+                if (child.getFirstChildNode() != null) {
+                    if (child.getFirstChildNode().getElementType().equals(XCSTypes.COLLECTION_EVENT)) {
+                        if (child.getPsi().getChildren()[0].getChildren().length != 0) {
+                            lastChildNode = child.getPsi().getChildren()[0].getChildren()[child.getPsi().getChildren()[0].getChildren().length - 1].getNode();
+                        } else {
+                            return;
+                        }
+                    }
+                }
+            }
+            if (lastChildNode != null) {
+                lastChildNode.addChild(XCSElementFactory.createCRLF(project).getNode());
+            }
+            XCSElementFactory.createPropertyCe(Objects.requireNonNull(lastChildNode), "VfeiName", key);
+            // Move to where the property has been created
+            ((Navigatable) Objects.requireNonNull(lastChildNode).getTreeNext().getPsi().getNavigationElement()).navigate(true);
+            Objects.requireNonNull(FileEditorManager.getInstance(project).getSelectedTextEditor()).getCaretModel().moveCaretRelatively(2, 0, false, false, false);
+        });
+    }
+
+}
